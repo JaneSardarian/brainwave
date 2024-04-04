7ceaa62c1d12a1a59af8a727dd38076946f29a1d
Jane Sardarian <sardarianjane@gmail.com>
2024-04-04 13:15:46 +0400

    I added animations to the Hero page

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -1,9 +1,13 @@
+import { ScrollParallax } from 'react-just-parallax'
+import { useRef } from 'react'
 import Section from './Section'
 import Button from './Button'
-import curve from '../assets/hero/curve.png'
-import robot from '../assets/hero/robot.jpg'
-import heroBackground from '../assets/hero/hero-background.jpg'
+import { curve, robot, heroBackground } from '../assets/index.js'
+import { BackgroundCircles, BottomLine, Gradient } from './design/Hero.jsx'
+import { heroIcons } from '../constants/index.js'
+
 const Hero = () => {
+  const parallaxRef = useRef(null)
   return (
     <Section
       className='pt-[12rem] -mt-[5.25rem]'
@@ -12,10 +16,10 @@ const Hero = () => {
       customPadding
       id='hero'
     >
-      <div className='container relative'>
+      <div className='container relative' ref={parallaxRef}>
         <div className='relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]'>
           <h1 className='h1 mb-6'>
-            Explore the Possibilities of AI Chatting with Brainwave
+            Explore the Possibilities of AI Chatting with{' '}
             <span className='inline-block relative'>
               Brainwave
               <img
@@ -47,8 +51,19 @@ const Hero = () => {
                   height={490}
                   alt='AI'
                 />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className='hidden absolute -left-[5.5rem] bottom-[7.5rem]  px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex'>
+                    {heroIcons.map((icon, index) => (
+                      <li className='p-5' key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
           <div className='absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]'>
             <img
@@ -59,6 +74,7 @@ const Hero = () => {
               alt='hero'
             />
           </div>
+          <BackgroundCircles />
         </div>
       </div>
     </Section>
--- my-project/src/components/Section.jsx
+++ my-project/src/components/Section.jsx
@@ -11,7 +11,9 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`relative ${
+      className={`
+      relative
+      ${
         customPadding ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl: py-40' : ''} ${
           className || ''
